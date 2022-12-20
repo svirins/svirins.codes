b83047e934d2c081aa04eb52be8404a6af2a4444
svirins <svirins@gmail.com>
2022-12-20 19:54:15 +0200

    refactor(2 of 2): bump to nextjs-13

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -13,17 +13,17 @@ export default function NavItem({ href, text }: INavItem) {
   const router = useRouter();
 
   return (
-    <NextLink href={href}>
-      <a
-        className={cn(
-          getActiveStatus(href, router.asPath)
-            ? 'font-semibold text-gray-800 dark:text-gray-200 '
-            : 'font-medium text-gray-600 dark:text-gray-400',
-          'hidden md:inline-block  transition-all pr-4 duration-150 hover:text-gray-800 dark:hover:text-gray-200 ease-in-out text-lg'
-        )}
-      >
-        <span className="capsize">{text}</span>
-      </a>
-    </NextLink>
+    (<NextLink
+      href={href}
+      className={cn(
+        getActiveStatus(href, router.asPath)
+          ? 'font-semibold text-gray-800 dark:text-gray-200 '
+          : 'font-medium text-gray-600 dark:text-gray-400',
+        'hidden md:inline-block  transition-all pr-4 duration-150 hover:text-gray-800 dark:hover:text-gray-200 ease-in-out text-lg'
+      )}>
+
+      <span className="capsize">{text}</span>
+
+    </NextLink>)
   );
 }
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -10,18 +10,18 @@ export default function PostPreview({
   tags
 }: Partial<IPost>) {
   return (
-    <Link href={`/blog/${slug}`}>
-      <a className=" w-full  duration-150 ease-in-out py-4">
-        <div className="w-full">
-          <Tags tags={tags} />
-          <h3 className="text-xl md:text-2xl font-medium text-left text-gray-900  hover:text-active dark:hover:text-active dark:text-gray-100">
-            {title}
-          </h3>
-          <p className="text-gray-700 dark:text-gray-400 md:text-lg">
-            {excerpt}
-          </p>
-        </div>
-      </a>
-    </Link>
+    (<Link href={`/blog/${slug}`} className=" w-full  duration-150 ease-in-out py-4">
+
+      <div className="w-full">
+        <Tags tags={tags} />
+        <h3 className="text-xl md:text-2xl font-medium text-left text-gray-900  hover:text-active dark:hover:text-active dark:text-gray-100">
+          {title}
+        </h3>
+        <p className="text-gray-700 dark:text-gray-400 md:text-lg">
+          {excerpt}
+        </p>
+      </div>
+
+    </Link>)
   );
 }
