3966b495f412e57190b430e8d19a8438dd547bf7
svirins <svirins@gmail.com>
2023-01-29 19:19:17 +0300

    revert(tailwind): remove cssnano

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './ui/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: 'class',
   theme: {
