ff10c917a35e5b9672f6451f9cf84be69c27cd34
svirins <svirins@gmail.com>
2023-02-02 16:52:47 +0300

    refactor(sanity): add code block to portable text

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
-
+import { codeInput } from '@sanity/code-input';
 export default defineConfig({
   name: 'default',
   title: 'svirins-codes-studio',
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: 'q60wk43i',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), codeInput()],
 
   schema: {
     types: schemaTypes
