f4b4ffbc8aff24141ef1570d55f72229ea9e7e3c
svirins <svirins@gmail.com>
2023-02-09 14:07:09 +0300

    fix(links): nested issue and styling

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -10,17 +10,15 @@ export default function PostPreview({
   tags
 }: Partial<IPost>) {
   return (
-    <Link
-      href={`/blog/${slug}`}
-      className=" w-full  duration-150 ease-in-out py-4"
-    >
-      <div className="w-full">
-        <Tags tags={tags!} />
-        <h3 className="text-xl md:text-2xl font-medium text-left text-gray-900  hover:text-active dark:hover:text-active dark:text-gray-100">
-          {title}
-        </h3>
-        <p className="text-gray-700 dark:text-gray-400 md:text-lg">{excerpt}</p>
-      </div>
-    </Link>
+    <div className='w-full py-4 text-left'>
+      <Tags tags={tags!} />
+      <Link
+        href={`/blog/${slug}`}
+        className='link-underline link-underline-gradient text-xl md:text-2xl font-medium  text-gray-900   dark:text-gray-100'
+      >
+        {title}
+      </Link>
+      <p className='text-gray-700 dark:text-gray-400 md:text-lg'>{excerpt}</p>
+    </div>
   );
 }
--- components/SnippetPreview.tsx
+++ components/SnippetPreview.tsx
@@ -10,20 +10,19 @@ export default function SnippetPreview({
   ...rest
 }: Partial<ISnippet>) {
   return (
-    <Link
-      href={`/snippets/${slug}`}
-      className="p-4 w-full border border-gray-400 dark:border-gray-700 rounded  transition-all   hover:border-active/50 dark:hover:border-active/50 duration-150 ease-in-out [&>h3]:  [&>h3]:hover:text-active"
-      {...rest}
-    >
-      <div className="pb-2 float-right  scale-125 md:scale-150 ml-4 origin-centers pr-2 pt-2">
+    <div className='p-4 w-full border border-gray-400 dark:border-gray-700 rounded'>
+      <div className='pb-2 float-right  scale-125 md:scale-150 ml-4 origin-centers pr-2 pt-2'>
         <SnippetIcon iconTitle={iconTitle!} />
       </div>
-      <h3 className="text-xl md:text-2xl font-medium text-left text-gray-900  hover:text-active dark:hover:text-active dark:text-gray-100">
+      <Link
+        href={`/snippets/${slug}`}
+        className='link-underline link-underline-gradient text-xl md:text-2xl font-medium  text-gray-900   dark:text-gray-100'
+      >
         {title}
-      </h3>
-      <p className="text-gray-700 dark:text-gray-400 pt-2  md:text-lg">
+      </Link>
+      <p className='text-gray-700 dark:text-gray-400 pt-2  md:text-lg'>
         {description}
       </p>
-    </Link>
+    </div>
   );
 }
