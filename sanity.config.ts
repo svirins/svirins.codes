4912ab097bb50873adf0b49930abeb9e71c8a679
svirins <svirins@gmail.com>
2023-02-14 23:05:48 +0300

    feat(openai): dall-e image asset for sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,14 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 import { codeInput } from '@sanity/code-input';
+import { openaiImageAsset } from 'sanity-plugin-asset-source-openai';
+import { loadEnvConfig } from '@next/env';
+
+const dev = process.env.NODE_ENV !== 'production';
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error });
+
+const openaiAPIkey = process.env.OPENAI_API_KEY as string;
+
 export default defineConfig({
   name: 'default',
   title: 'svirins-codes-studio',
@@ -10,7 +18,14 @@ export default defineConfig({
   projectId: 'q60wk43i',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), codeInput()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    codeInput(),
+    openaiImageAsset({
+      API_KEY: openaiAPIkey
+    })
+  ],
 
   schema: {
     types: schemaTypes
