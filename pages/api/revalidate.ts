ad2961d674af7d86cdd7b35bc8811b39d33e4193
svirins <svirins@gmail.com>
2023-02-21 16:09:52 +0300

    refactor: bump to next/canary

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -3,7 +3,7 @@ import { parseBody } from 'next-sanity/webhook';
 import { NextApiRequest, NextApiResponse } from 'next';
 export { config } from 'next-sanity/webhook';
 
-export const runtime = 'edge';
+export const runtime = 'node';
 
 async function handler(
   request: NextApiRequest,
